0479e7f34336ecdfe6078a6995cb065ec99e6753
Jeremy Stober <stober@gmail.com>
2012-05-24 15:23:29 -0500

    Removed pqueue. Now found in pqueue module.

--- src/__init__.py
+++ src/__init__.py
@@ -6,57 +6,7 @@ Date: Thursday, May 17 2012
 Description: A* search in Python.
 """
 
-import heapq as hq
-import itertools
-
-class pqueue:
-    """
-    A priority queue with fast member checking and variable tie
-    breaking (LIFO or FIFO). Updated using the recipe from the heapq
-    documentation.
-    """
-
-    def __init__(self, policy = "LIFO"):
-        if policy == 'LIFO':
-            self.step = -1
-        else: # policy == 'FIFO'
-            self.step = 1
-
-        self.counter = itertools.count() # step only allowed in 2.7
-        self.heap = []
-        self.entries = {}
-        self.removed_key = '<REMOVED>'
-
-    def push(self, pri, key):
-        if key in self.entries:
-            self.remove(key)
-        cnt = next(self.counter) * self.step
-        entry = [pri, cnt, key]
-        self.entries[key] = entry
-        hq.heappush(self.heap, entry)
-
-    def __contains__(self, key):
-        return self.entries.has_key(key)
-
-    def __len__(self):
-        return len(self.entries)
-
-    def remove(self, key):
-        """
-        Removes all items from the priority queue.
-        """
-
-        entry = self.entries.pop(key)
-        entry[-1] = self.removed_key
-
-    def pop(self):
-        pri, cnt, key = hq.heappop(self.heap)
-
-        while key == self.removed_key:
-            pri, cnt, key = hq.heappop(self.heap)
-
-        del self.entries[key]
-        return key
+from pqueue import pqueue
 
 def reconstruct(partialmap, node):
     if partialmap.has_key(node):
